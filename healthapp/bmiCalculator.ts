e30f5212f564c3151c36e36ad3416635b65539f1
Kisimoni Aubain <kisimoniaubain@gmail.com>
2026-08-17 09:26:22 +0300

    Exercise 9.1 Complete BMI calculator exercise

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,20 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+
+  if (bmi < 18.5) {
+    return "Underweight range";
+  }
+
+  if (bmi < 25) {
+    return "Normal range";
+  }
+
+  if (bmi < 30) {
+    return "Overweight range";
+  }
+
+  return "Obese range";
+};
+
+console.log(calculateBmi(180, 74));
