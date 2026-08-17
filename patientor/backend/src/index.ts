b62c81ca380c54ea02f3badd6532ae2d21b74d09
Kisimoni Aubain <kisimoniaubain@gmail.com>
2026-08-17 12:42:02 +0300

    Exercise 9.9: Initialize Patientor backend

---
+++ patientor/backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
