3ddf41296bc2be9ed5e31284713a2e7c28ca8106
Kisimoni Aubain <kisimoniaubain@gmail.com>
2026-08-17 14:38:57 +0300

    Exercise 9.10: Connect Patientor frontend to backend

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 3001;
+
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
